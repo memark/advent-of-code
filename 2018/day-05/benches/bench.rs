d167289ea21d1c5f80bef7f53508d77f096d92dc
Magnus Markling <magnus@markling.com>
2022-11-27 15:18:54 +0100

    improve algorithm

--- 2018/day-05/benches/bench.rs
+++ 2018/day-05/benches/bench.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code, unused_imports)]
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use day_05::{improve_polymer_and_react, react};
+use day_05::{improve_polymer_and_react, react, react_2};
 use std::{fs, time::Duration};
 
 fn bench_react(c: &mut Criterion) {
@@ -10,6 +10,15 @@ fn bench_react(c: &mut Criterion) {
 
     c.bench_function("react", |b| {
         b.iter(|| react(file));
+    });
+}
+
+fn bench_react_2(c: &mut Criterion) {
+    let file = fs::read_to_string("input").unwrap();
+    let file = file.trim();
+
+    c.bench_function("react_2", |b| {
+        b.iter(|| react_2(file));
     });
 }
 
@@ -23,7 +32,7 @@ fn bench_improve_polymer_and_react(c: &mut Criterion) {
 }
 
 // criterion_group!(benches, bench_react, bench_improve_polymer_and_react);
-criterion_group!(benches, bench_react);
+criterion_group!(benches, bench_react, bench_react_2);
 // criterion_group! {
 //   name = benches;
 //   config = Criterion::default()
--- 2018/day-05/src/lib.rs
+++ 2018/day-05/src/lib.rs
@@ -38,8 +38,47 @@ pub fn react(polymer: &str) -> String {
     result
 }
 
+pub fn react_2(polymer: &str) -> String {
+    let mut result = polymer.to_owned();
+    let mut changed = true;
+
+    while changed {
+        changed = false;
+
+        let chars_count = result.chars().count();
+
+        let mut new_result = "".to_owned();
+        let mut i = 0;
+
+        while i < chars_count {
+            if i < chars_count - 1
+                && should_react(
+                    result[i..=i].chars().next().unwrap(),
+                    result[i + 1..=i + 1].chars().next().unwrap(),
+                )
+            {
+                changed = true;
+                i += 2;
+            } else {
+                new_result.push_str(&result[i..=i]);
+                i += 1;
+            }
+        }
+        result = new_result;
+    }
+    result
+}
+
+// react_3: use vector of chars all the way?
+
 fn remove_units(polymer: &str, c: char) -> String {
     polymer.replace([c, switch_case(c)], "")
+}
+
+fn should_react(a: char, b: char) -> bool {
+    a.to_ascii_lowercase() == b.to_ascii_lowercase()
+        && ((a.is_ascii_lowercase() && b.is_ascii_uppercase())
+            || (a.is_ascii_uppercase() && b.is_ascii_lowercase()))
 }
 
 fn switch_case(c: char) -> char {
@@ -66,6 +105,19 @@ mod tests {
     #[test]
     fn react_larger_example() {
         assert_eq!(react("dabAcCaCBAcCcaDA"), "dabCBAcaDA");
+    }
+
+    #[test]
+    fn react_2_examples() {
+        assert_eq!(react_2("aA"), "");
+        assert_eq!(react_2("abBA"), "");
+        assert_eq!(react_2("abAB"), "abAB");
+        assert_eq!(react_2("aabAAB"), "aabAAB");
+    }
+
+    #[test]
+    fn react_2_larger_example() {
+        assert_eq!(react_2("dabAcCaCBAcCcaDA"), "dabCBAcaDA");
     }
 
     #[test]
