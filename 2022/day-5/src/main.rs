afb6616c01088dc596a4da61e46cb99eb5211936
Magnus Markling <magnus@markling.com>
2022-12-05 07:53:01 +0100

    refactor

--- 2022/day-5/src/main.rs
+++ 2022/day-5/src/main.rs
@@ -1,13 +1,9 @@
-#![allow(
-    dead_code,
-    unreachable_code,
-    unused_imports,
-    unused_variables,
-    unused_mut
-)]
+#[macro_use]
+extern crate lazy_static;
 
 use itertools::Itertools;
-use std::fs;
+use regex::Regex;
+use std::{fs, num::ParseIntError, str::FromStr};
 
 fn main() {
     println!("Part 1: {}", solve_part_1(&file("input")));
@@ -15,206 +11,122 @@ fn main() {
 }
 
 fn solve_part_1(input: &str) -> String {
-    // Parse
-    //   split på \n\n
-    //   läs ut stacks mha position (regex?)
-    //   moves också position (flera siffror)
-
-    dbg!(&input);
-
-    let (a, b) = input.split_once("\n\n").unwrap();
-    let a = a.lines().collect_vec();
-    let b = b.lines().collect_vec();
-    // dbg!(&a);
-    // dbg!(&b);
-
-    let num_stacks = &a
-        .iter()
-        .last()
-        .unwrap()
-        .split_ascii_whitespace()
-        .last()
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
-    dbg!(&num_stacks);
-
-    let mut stack_rows = a.clone();
-    stack_rows.pop();
-    dbg!(&stack_rows);
-
-    let mut stacks = Vec::new();
-
-    for stack_index in 0..*num_stacks {
-        dbg!(&stack_index);
-
-        let mut stack = Vec::new();
-
-        for row_index in (0..stack_rows.len()).rev() {
-            let stack_row = stack_rows[row_index];
-            let pos = 1 + stack_index * 4;
-            let c = &stack_row.chars().nth(pos).unwrap();
-            if *c != ' ' {
-                // dbg!(&pos);
-                // dbg!(&c);
-                stack.push(*c);
-            }
-        }
-        // dbg!(&stack);
-
-        stacks.push(stack);
-        // for stack_row in &stack_rows {
-        //     //
-        //     let pos = 1 + stack_index * 4;
-        //     dbg!(&pos);
-        //     let c = &stack_row.chars().nth(pos).unwrap();
-        //     dbg!(&c);
-        // }
-    }
-    dbg!(&stacks);
-
-    // let mut s1 = vec!['Z', 'N'];
-    // let mut s2 = vec!['M', 'C', 'D'];
-    // let mut s3 = vec!['P'];
-
-    // let mut ss = vec![s1, s2, s3];
-
-    let steps = &b
-        .iter()
-        .map(|l| l.split(' ').collect_vec())
-        .map(|l| {
-            (
-                l[1].parse().unwrap(),
-                l[3].parse().unwrap(),
-                l[5].parse().unwrap(),
-            )
-        })
-        .collect::<Vec<(usize, usize, usize)>>();
-    // dbg!(&steps);
-
-    // flytta crates
-    //   >1 innebär separata steg!
-
-    for (count, from, to) in steps {
-        for i in 0..*count {
-            //
-            // dbg!((count, from, to));
-            let t = stacks[from - 1].pop().unwrap();
-            stacks[to - 1].push(t);
-        }
-    }
-    dbg!(&stacks);
-
-    let top = stacks.iter().map(|s| s.last().unwrap()).collect::<String>();
-    dbg!(&top);
-
-    top
+    solve_with_strategy(input, CrateMover9000)
 }
 
 fn solve_part_2(input: &str) -> String {
-    // Parse
-    //   split på \n\n
-    //   läs ut stacks mha position (regex?)
-    //   moves också position (flera siffror)
+    solve_with_strategy(input, CrateMover9001)
+}
 
-    dbg!(&input);
-
+fn solve_with_strategy(input: &str, crate_mover: impl CrateMover) -> String {
     let (a, b) = input.split_once("\n\n").unwrap();
-    let a = a.lines().collect_vec();
-    let b = b.lines().collect_vec();
-    // dbg!(&a);
-    // dbg!(&b);
 
-    let num_stacks = &a
-        .iter()
-        .last()
-        .unwrap()
+    let stack_rows = a.lines().dropping_back(1).collect_vec();
+    let stack_header = a.lines().last().unwrap();
+
+    let step_rows = b.lines().collect_vec();
+
+    let num_stacks = get_num_stacks(stack_header);
+
+    let stacks = parse_stacks(num_stacks, &stack_rows);
+    let steps = parse_steps(&step_rows);
+
+    let stacks = crate_mover.operate(&steps, &stacks);
+
+    let top = stacks.iter().map(|s| s.last().unwrap()).collect::<String>();
+    top
+}
+
+fn get_num_stacks(stack_header: &str) -> usize {
+    stack_header
         .split_ascii_whitespace()
         .last()
         .unwrap()
-        .parse::<usize>()
-        .unwrap();
-    dbg!(&num_stacks);
+        .parse()
+        .unwrap()
+}
 
-    let mut stack_rows = a.clone();
-    stack_rows.pop();
-    dbg!(&stack_rows);
+fn parse_stacks(num_stacks: usize, stack_rows: &[&str]) -> Vec<Vec<char>> {
+    (0..num_stacks)
+        .map(|stack_index| {
+            stack_rows
+                .iter()
+                .rev()
+                .filter_map(|stack_row| {
+                    let pos = 1 + stack_index * 4;
+                    let c = stack_row.chars().nth(pos).unwrap();
+                    if c != ' ' {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                })
+                .collect_vec()
+        })
+        .collect_vec()
+}
 
-    let mut stacks = Vec::new();
+fn parse_steps(b: &[&str]) -> Vec<Step> {
+    b.iter().map(|l| l.parse().unwrap()).collect_vec()
+}
 
-    for stack_index in 0..*num_stacks {
-        dbg!(&stack_index);
+struct Step {
+    count: usize,
+    from: usize,
+    to: usize,
+}
 
-        let mut stack = Vec::new();
+impl FromStr for Step {
+    type Err = ParseIntError;
 
-        for row_index in (0..stack_rows.len()).rev() {
-            let stack_row = stack_rows[row_index];
-            let pos = 1 + stack_index * 4;
-            let c = &stack_row.chars().nth(pos).unwrap();
-            if *c != ' ' {
-                // dbg!(&pos);
-                // dbg!(&c);
-                stack.push(*c);
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+        }
+        let caps = RE.captures(s).unwrap();
+        Ok(Step {
+            count: caps[1].parse().unwrap(),
+            from: caps[2].parse().unwrap(),
+            to: caps[3].parse().unwrap(),
+        })
+    }
+}
+
+trait CrateMover {
+    fn operate(&self, steps: &[Step], stacks: &[Vec<char>]) -> Vec<Vec<char>>;
+}
+
+struct CrateMover9000;
+impl CrateMover for CrateMover9000 {
+    fn operate(&self, steps: &[Step], stacks: &[Vec<char>]) -> Vec<Vec<char>> {
+        let mut stacks = stacks.to_vec();
+        for Step { count, from, to } in steps {
+            for _ in 0..*count {
+                let t = stacks[from - 1].pop().unwrap();
+                stacks[to - 1].push(t);
             }
         }
-        // dbg!(&stack);
-
-        stacks.push(stack);
-        // for stack_row in &stack_rows {
-        //     //
-        //     let pos = 1 + stack_index * 4;
-        //     dbg!(&pos);
-        //     let c = &stack_row.chars().nth(pos).unwrap();
-        //     dbg!(&c);
-        // }
+        stacks.to_vec()
     }
-    dbg!(&stacks);
+}
 
-    // let mut s1 = vec!['Z', 'N'];
-    // let mut s2 = vec!['M', 'C', 'D'];
-    // let mut s3 = vec!['P'];
-
-    // let mut ss = vec![s1, s2, s3];
-
-    let steps = &b
-        .iter()
-        .map(|l| l.split(' ').collect_vec())
-        .map(|l| {
-            (
-                l[1].parse().unwrap(),
-                l[3].parse().unwrap(),
-                l[5].parse().unwrap(),
-            )
-        })
-        .collect::<Vec<(usize, usize, usize)>>();
-    // dbg!(&steps);
-
-    // flytta crates
-    //   >1 innebär separata steg!
-
-    for (count, from, to) in steps {
-        let mut temp = Vec::new();
-        for i in 0..*count {
-            // dbg!((count, from, to));
-            let t = stacks[from - 1].pop().unwrap();
-            temp.push(t);
+struct CrateMover9001;
+impl CrateMover for CrateMover9001 {
+    fn operate(&self, steps: &[Step], stacks: &[Vec<char>]) -> Vec<Vec<char>> {
+        let mut stacks = stacks.to_vec();
+        for Step { count, from, to } in steps {
+            let mut temp = Vec::new();
+            for _ in 0..*count {
+                let t = stacks[from - 1].pop().unwrap();
+                temp.push(t);
+            }
+            for _ in 0..*count {
+                let t = temp.pop().unwrap();
+                stacks[to - 1].push(t);
+            }
         }
-        // dbg!(&temp);
-        // temp.reverse();
-        // dbg!(&temp);
-        for i in 0..*count {
-            // dbg!((count, from, to));
-            let t = temp.pop().unwrap();
-
-            stacks[to - 1].push(t);
-        }
+        stacks.to_vec()
     }
-    dbg!(&stacks);
-
-    let top = stacks.iter().map(|s| s.last().unwrap()).collect::<String>();
-    dbg!(&top);
-
-    top
 }
 
 fn file(path: &str) -> String {
