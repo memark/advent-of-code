08dacab4fc98446803327829b4f00f48382027ca
Magnus Markling <magnus@markling.com>
2023-05-05 00:06:52 +0200

    2019 05 (rs) wip

---
+++ 2019/day-05/src/main.rs
@@ -0,0 +1,248 @@
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use itertools::Itertools;
+use std::{ num::ParseIntError, fs, process::Output };
+
+fn main() {
+    println!("Part 1: {:?}", part1());
+    println!("Part 2: {:?}", part2());
+}
+
+fn part1() -> Int {
+    0
+}
+
+fn part2() -> Int {
+    0
+}
+
+fn run_program(mut mem: Vec<Int>) -> Vec<Int> {
+    let mut ip = 0;
+    loop {
+        let (i, ip_delta) = Instruction::from_ints(&mem[ip..]);
+        if i == Halt {
+            break;
+        }
+        mem = i.process(mem);
+        ip += ip_delta;
+    }
+    mem
+}
+
+fn run_program_with_io(mut mem: Vec<Int>, mut input: Vec<Int>) -> Vec<Int> {
+    let mut output = vec![];
+    let mut ip = 0;
+    loop {
+        let (i, ip_delta) = Instruction::from_ints(&mem[ip..]);
+        if i == Halt {
+            break;
+        }
+        (mem, input, output) = i.process_with_io(mem, input, output);
+        ip += ip_delta;
+    }
+    output
+}
+
+type Int = usize;
+
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Add {
+        src1: Int,
+        src2: Int,
+        dst: Int,
+    },
+    Multiply {
+        src1: Int,
+        src2: Int,
+        dst: Int,
+    },
+    Input {
+        dst: Int,
+    },
+    Output {
+        src: Int,
+    },
+    Halt,
+}
+use Instruction::*;
+
+impl Instruction {
+    fn from_ints(ints: &[Int]) -> (Self, Int) {
+        match ints[0] {
+            1 => (Self::Add { src1: ints[1], src2: ints[2], dst: ints[3] }, 4),
+            2 => (Self::Multiply { src1: ints[1], src2: ints[2], dst: ints[3] }, 4),
+            3 => (Self::Input { dst: ints[1] }, 2),
+            4 => (Self::Output { src: ints[1] }, 2),
+            99 => (Self::Halt, 1),
+            _ => panic!("Unknown opcode"),
+        }
+    }
+
+    fn process(self, mut mem: Vec<Int>) -> Vec<Int> {
+        match self {
+            Self::Add { src1, src2, dst } => {
+                mem[dst] = mem[src1] + mem[src2];
+            }
+            Self::Multiply { src1, src2, dst } => {
+                mem[dst] = mem[src1] * mem[src2];
+            }
+            Self::Halt => {}
+            _ => unimplemented!(),
+        }
+        mem
+    }
+
+    fn process_with_io(
+        self,
+        mut mem: Vec<Int>,
+        mut input: Vec<Int>,
+        mut output: Vec<Int>
+    ) -> (Vec<Int>, Vec<Int>, Vec<Int>) {
+        match self {
+            Self::Add { src1, src2, dst } => {
+                mem[dst] = mem[src1] + mem[src2];
+            }
+            Self::Multiply { src1, src2, dst } => {
+                mem[dst] = mem[src1] * mem[src2];
+            }
+            Self::Input { dst } => {
+                mem[dst] = input.remove(0);
+            }
+            Self::Output { src } => output.push(mem[src]),
+            Self::Halt => {}
+        }
+        (mem, input, output)
+    }
+}
+
+fn parse_ints(s: &str) -> Vec<Int> {
+    if s.is_empty() {
+        vec![]
+    } else {
+        s.split(',')
+            .map(|ss| ss.trim().parse().unwrap())
+            .collect_vec()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
+    #[rstest]
+    fn parses_ints_with_newlines() {
+        let input = "1,9,10,3,
+        2,3,11,0,
+        99,
+        30,40,50";
+
+        let actual = parse_ints(input);
+
+        assert_eq!(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], actual);
+    }
+
+    #[rstest]
+    fn parses_ints_empty() {
+        let input = "";
+
+        let actual = parse_ints(input);
+
+        assert_eq!(vec![0; 0], actual);
+    }
+
+    #[rstest]
+    #[case("1,9,10,3,2,3,11,0,99,30,40,50", (Instruction::Add { src1: 9, src2: 10, dst: 3 }, 4))]
+    #[case("2,3,11,0,99,30,40,50", (Instruction::Multiply { src1: 3, src2: 11, dst: 0 }, 4))]
+    #[case("3,50", (Instruction::Input { dst: 50 }, 2))]
+    #[case("4,50", (Instruction::Output { src: 50 }, 2))]
+    #[case("99,30,40,50", (Instruction::Halt, 1))]
+    fn parses_opcode(#[case] input: &str, #[case] expected: (Instruction, Int)) {
+        assert_eq!(expected, Instruction::from_ints(&parse_ints(input)));
+    }
+
+    #[test]
+    #[should_panic(expected = "Unknown opcode")]
+    fn panics_on_unknown_opcode() {
+        let input = "123,2,3,11,0,99,30,40,50";
+
+        let actual = Instruction::from_ints(&parse_ints(input));
+    }
+
+    #[rstest]
+    #[case(
+        Instruction::Add {
+            src1: 9,
+            src2: 10,
+            dst: 3,
+        },
+        "1,9,10,3,2,3,11,0,99,30,40,50",
+        "1,9,10,70,2,3,11,0,99,30,40,50"
+    )]
+    #[case(
+        Instruction::Multiply { src1: 3, src2: 11, dst: 0 },
+        "1,9,10,70,2,3,11,0,99,30,40,50",
+        "3500,9,10,70,2,3,11,0,99,30,40,50"
+    )]
+    fn processes_instruction(
+        #[case] instruction: Instruction,
+        #[case] mem: &str,
+        #[case] expected: &str
+    ) {
+        assert_eq!(expected, instruction.process(parse_ints(mem)).iter().join(","));
+    }
+
+    #[rstest]
+    #[case(Instruction::Input { dst: 0 }, "0", "123", "123", "", "")]
+    #[case(Instruction::Output { src: 0 }, "123", "", "123", "", "123")]
+    fn processes_instruction_with_io(
+        #[case] instruction: Instruction,
+        #[case] mem: &str,
+        #[case] input: &str,
+        #[case] expected_mem: &str,
+        #[case] expected_input: &str,
+        #[case] expected_output: &str
+    ) {
+        let (actual_mem, actual_input, actual_output) = instruction.process_with_io(
+            parse_ints(mem),
+            parse_ints(input),
+            vec![]
+        );
+
+        assert_eq!(actual_mem.iter().join(","), expected_mem);
+        assert_eq!(actual_input.iter().join(","), expected_input);
+        assert_eq!(actual_output.iter().join(","), expected_output);
+    }
+
+    #[rstest]
+    #[case("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50")]
+    #[case("1,0,0,0,99", "2,0,0,0,99")]
+    #[case("2,3,0,3,99", "2,3,0,6,99")]
+    #[case("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+    #[case("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+    fn runs_program(#[case] input: &str, #[case] expected: &str) {
+        let actual = run_program(parse_ints(input)).iter().join(",");
+
+        assert_eq!(actual, expected)
+    }
+
+    #[rstest]
+    #[case("3,0,4,0,99", "123", "123")]
+    fn runs_program_with_io(#[case] mem: &str, #[case] input: &str, #[case] expected: &str) {
+        let actual = run_program_with_io(parse_ints(mem), parse_ints(input)).iter().join(",");
+
+        assert_eq!(actual, expected)
+    }
+
+    // #[test]
+    // fn runs_part1() {
+    //     assert_eq!(5346030, part1());
+    // }
+
+    // #[test]
+    // fn runs_part2() {
+    //     assert_eq!(513116, part2());
+    // }
+}
