e068b8439201d5494e481aa7602ba3aca3e9cc75
Magnus Markling <magnus@markling.com>
2023-05-14 10:00:37 +0200

    add intcode-computer-cli

---
+++ 2019/intcode-computer-cli/src/main.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+use intcode_computer::{ run_program, state::State, ints_to_hashmap, parse_ints };
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// memory (instructions and data) as a comma-separated list of ints, e.g. "3,0,4,0,99"
+    #[arg()]
+    memory: String,
+
+    /// input as a comma-separated list of ints, e.g. "1,2,3"
+    #[arg()]
+    input: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let memory = ints_to_hashmap(parse_ints(&args.memory));
+    let input = parse_ints(&args.input);
+
+    let state = State::with_input(memory, input);
+    let output = run_program(state).output;
+
+    println!("{output:?}");
+}
